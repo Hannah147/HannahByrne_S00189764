eca7d9c618feffc80e838ca1b12d0189b516e8c0
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2020-05-13 10:10:37 +0100

    Created Phone class and added properties

---
+++ HannahByrne_S00189764/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HannahByrne_S00189764
+{
+    class Phone
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+    }
+}
