d913d75b852474f8943313efc21a137a13edc6d5
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2020-05-13 10:14:58 +0100

    Added IncreasePrice method

--- HannahByrne_S00189764/Phone.cs
+++ HannahByrne_S00189764/Phone.cs
@@ -13,5 +13,13 @@ namespace HannahByrne_S00189764
         public string OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
+
+        public double IncreasePrice(double percentage)
+        {
+            Price = Price + (Price * percentage);
+            return Price;
+        }
     }
+
+    
 }
